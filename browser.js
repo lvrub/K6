b71e6f224a1f00048b3632656d5f676e5e6091fc
Leonid <lvrub@users.noreply.github.com>
2024-09-10 16:36:32 +0300

    browser

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 import { browser } from 'k6/browser';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
     scenarios: {
@@ -15,22 +15,22 @@ export const options = {
   }
 
 export default async function () {
-    const page = browser.newPage();
-    await (await page).goto('https://test.k6.io/my_messages.php');
+    const page = await browser.newPage();
+    await page.goto('https://test.k6.io/my_messages.php');
 
     check(page, {
-        'check page header': (await page).locator('body > h2').textContent() === 'Unauthorized'
+        'check page header first': await page.locator('body > h2').textContent() === 'Unauthorized'
     });
 
-    (await page).locator('input[name="login"]').type('admin');
-    (await page).locator('input[name="password"]').type('123');
-    (await page).locator('input[type=submit]').click();
-    (await page).waitForFunction();
+    await page.locator('input[name="login"]').type('admin');
+    await page.locator('input[name="password"]').type('123');
+    await page.locator('input[type=submit]').click();
+    await page.waitForNavigation();
 
-    (await page).screenshot({ path: 'screenshots/2-authenticated.png' });
+    await page.screenshot({ path: 'screenshots/2-authenticated.png' });
 
     check(page, {
-        'check page header':(await page).locator('body > h2').textContent() === 'Welcome, admin!'
+        'check page header second':await page.locator('body > h2').textContent() === 'Welcome, admin!'
     });
 
 }
